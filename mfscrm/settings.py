34998ede1f26e97cf49681277defcfe449df55d3
srujan68 <skunduru@unomaha.edu>
2022-10-21 18:41:39 -0500

    allowed host

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-nn7_+j34oxh=#*ln$727$e&*^%$g-=3#t0mi9yzblzznpo2ygh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
